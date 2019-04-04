d5fd0ff32bae49b0fd52cd55f39dfce18e4f3eeb
Andrew W. Hill <andrew@textile.io>
2019-04-04 15:42:34 -0700

    Andrew/update 1.1.0 (#2)
    * working rn-image-local
    
    * updates to latest sdk

--- src/Containers/Home.tsx
+++ src/Containers/Home.tsx
@@ -1,45 +1,44 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Dispatch } from 'redux'
-import { View, Text, Image, TouchableOpacity, Linking } from 'react-native'
+import { View, Text, Image, TouchableOpacity, Linking, Dimensions } from 'react-native'
 import { RootAction, RootState } from '../Redux/Types'
 import styles from './Styles'
 import { NodeState } from '@textile/react-native-sdk'
+import TextileImage from '@textile/react-native-textile-image'
+
+const IPFS_PIN = 'QmTgtbb4LckHaXh1YhpNcBu48cFY8zgT1Lh49q7q7ksf3M/raster-generated/ipfs-logo-256-ice.png'
 
 class Home extends React.Component<StateProps> {
   render() {
     const previewText = !this.props.online ? 'waiting to come online...' : this.props.ipfsImage === undefined ? 'requesting ipfs hash...' : 'ipfs request complete'
+    // if (this.props.online) {
+    //   return this.renderPanZoom()
+    // }
     return (
       <View style={styles.container}>
         <View style={styles.header}>
           <Text style={styles.title}>{this.props.nodeState} local node</Text>
         </View>
         <View style={styles.ipfs}>
-          <TouchableOpacity
-            style={styles.imageView}
-            onPress={this.viewFile}
-          >
-            {this.ipfsImage()}
-            {this.props.ipfsImage && <Text style={styles.hashText}>QmTgtbb4LckHaXh1YhpNcBu48cFY8zgT1Lh49q7q7ksf3M</Text>}
-          </TouchableOpacity>
+
+            {this.props.ipfsImage && this.renderImage()}
+
           <Text style={styles.statusText}>{previewText}</Text>
         </View>
       </View>
     )
   }
-  viewFile = () => {
-    Linking.openURL('https://ipfs.io/ipfs/QmTgtbb4LckHaXh1YhpNcBu48cFY8zgT1Lh49q7q7ksf3M/raster-generated/ipfs-logo-256-ice.png')
-  }
-  ipfsImage = () => {
-    if (this.props.ipfsImage) {
-      return (
-        <Image
-          style={{width: 150, height: 150, resizeMode: 'cover'}}
-          source={{uri: this.props.ipfsImage}}
-        />
-      )
-    }
-    return (<View/>)
+
+  renderImage = () => {
+    return (
+      <TextileImage
+        style={{ width: 150, height: 150}}
+        target={IPFS_PIN}
+        ipfs={true}
+        resizeMode={'cover'}
+      />
+    )
   }
 }
 
@@ -55,8 +54,7 @@ const mapStateToProps = (state: RootState): StateProps => ({
 })
 
 const mapDispatchToProps = (dispatch: Dispatch<RootAction>): {} => {
-  return {
-  }
+  return {}
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home)
